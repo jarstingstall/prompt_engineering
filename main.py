eb642febb87792c891fb02c94eaaf78fb8cb80f2
jarstingstall <john.arstingstall@junglescout.com>
2024-01-12 09:35:25 -0500

    support user input questions

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI()
 
+user_input = input("What can I help you with today? ")
 chat_completion = client.chat.completions.create(
     messages=[
         {'role': 'system', 'content': 'You are a helpful assistant.'},
-        {'role': 'user', 'content': 'Which NFL team plays in Cincinnati?'},
+        {'role': 'user', 'content': user_input},
     ],
     model="gpt-3.5-turbo",
     temperature=0.5,
